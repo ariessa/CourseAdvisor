c95d2d4395f82c478a0d3b92a67691dba998f112
ariessa <ariessa@users.noreply.github.com>
2019-05-28 23:00:27 +0800

    Add files via upload

---
+++ courseAdvisor.pl
@@ -0,0 +1,299 @@
+% Project: Intelligent Course Advisor
+%
+%      By: Nurul Ariessa Binti Norramli (62219)
+%          Siti Azimah Binti Mohamed (63791)
+%          William Phedra (64191)
+%          Mohamad Sholehaddin Al-Hafiz Bin Mohamad (64016)
+
+% Facts for Band 1, 2
+course(1, 'TMF1414', 'Introduction to Programming', 4, 'a', 'None' , '').
+course(1, 'TMF1814', 'Discrete Mathematics', 4, 'a', 'None', '').
+course(1, 'TMF1014', 'System Analysis and Design', 4, 'a', 'None', '').
+course(1, 'MPU3222', 'Foundation of Entrepreneurship Cultivation', 2, 'a', 'None', '').
+course(1, 'PPD1041', 'Softskill and Basic Volunteerism', 1, 'a', 'None', '').
+course(1, 'PBI1112', 'Preparatory English 1', 2, 'a', 'None', '').
+course(2, 'TMF1874', 'Mathematics for Computing', 4, 'a', 'None', '').
+course(2, 'TMF1254', 'Communications and Computer Network', 4, 'a', 'None', '').
+course(2, 'TMF1434', 'Data_Structures_and_Algorithms', 4, 'a', 'TMF1414', 'Introduction to Programming').
+course(2, 'TMF1214', 'Computer_Architecture', 4, 'a', 'None', '').
+course(2, 'MPU3422', 'Systematic_Innovation_and_Innovative_Problem_Solving', 2, 'a', 'None', '').
+course(2, 'PPD1032', 'Credited_Co-Curricular', 2, 'a', 'None', '').
+course(2, 'PBI1122', 'Preparatory_English_2', 2, 'a', 'PBI1112', 'Preparatory English 1').
+course(3, 'TMF2034', 'Database_Concept_and_Design', 4, 'a', 'None', '').
+course(3, 'TME2413', 'Object_Oriented_Software_Development', 3, 'a', 'TMF1434', 'Data_Structures_and_Algorithms').
+course(3, 'TME2104', 'Web_Based_System_Development', 4, 'a', 'None', '').
+course(3, 'TMF2234', 'Operating_Systems', 4, 'a', 'None', '').
+course(3, 'PBI1102', 'Academic_English_1', 2, 'a', 'PBI1122', 'Preparatory_English_2').
+course(3, 'MPU3122', 'Islamic_and_Asian_Civilization', 2, 'a', 'None', '').
+course(4, 'TME2133', 'Software_Requirements_Engineering', 4, 'a', 'None', '').
+course(4, 'TMF2954', 'Java_Programming', 4, 'a', 'TMF1434', 'Data_Structures_and_Algorithms').
+course(4, 'TME2073', 'Intelligent_Systems', 4, 'a', 'TMF1434', 'Data_Structures_and_Algorithms').
+course(4, 'ABCXXXX', 'Elective_I', 3, 'a', 'None', '').
+course(4, 'PBI1102', 'English_for_Professional_Communication', 2, 'a', 'PBI1102', 'Academic_English_1').
+course(4, 'MPU3112', 'Ethnic_Relationship_in_Malaysia', 2, 'a', 'None', '').
+course(5, 'TMF3113', 'Project_Management', 3, 'a', 'None', '').
+course(5, 'TMF3012', 'Ethics_and_Professionalism', 2, 'a', 'None', '').
+course(5, 'TME3033', 'Expert_Systems', 3, 'a', 'TME2073', 'Intelligent_Systems').
+course(5, 'TME3413', 'Software_Engineering_Laboratory', 3, 'a', 'TME2413', 'Object_Oriented_Software_Development').
+course(5, 'TME3423', 'Human_Computer_Interaction', 3, 'a', 'None', '').
+course(5, 'ABCXXXX', 'Elective_II', 3, 'a', 'None', '').
+course(5, 'MPU3312', 'Malay_Language', 3, 'a', 'None', '').
+course(6, 'TMF39412', 'Industrial_Training', 12, 'a', 'TMF3012', 'Ethics_and_Professionalism').
+course(7, 'TMF4034', 'Technopreneurship_and_Product_Development', 4, 'a', 'None', '').
+course(7, 'TME4013', 'Formal_Methods', 3, 'a', 'TMF1814', 'Discrete Mathematics').
+course(7, 'TME4113', 'Data_Mining', 3, 'a', 'None', '').
+course(7, 'TMF4913', 'Final_Year_Project_I', 3, 'a', 'TMF3113', 'Project_Management').
+course(7, 'ABCXXXX', 'Elective_III', 3, 'a', 'None', '').
+course(8, 'TME4093', 'Advanced_Topics_in_Software_Engineering', 3, 'a', 'None', '').
+course(8, 'TMF4033', 'Software_Testing', 3, 'a', 'None', '').
+course(8, 'TME4433', 'Computer_Security', 3, 'a', 'None', '').
+course(8, 'TMF4935', 'Final_Year_Project_II', 5, 'a', 'TMF4913', 'Final_Year_Project_I').
+
+% Facts for Band 3
+course(1, 'TMF1414', 'Introduction to Programming', 4, 'b', 'None', '').
+course(1, 'TMF1814', 'Discrete Mathematics', 4, 'b', 'None', '').
+course(1, 'TMF1014', 'System Analysis and Design', 4, 'b', 'None', '').
+course(1, 'MPU3222', 'Foundation of Entrepreneurship Cultivation', 2, 'b', 'None', '').
+course(1, 'PBI1102', 'Academic_English_1', 2, 'b', 'None', '').
+course(1, 'PPD1041', 'Softskill and Basic Volunteerism', 1, 'b', 'None', '').
+course(2, 'TMF1874', 'Mathematics for Computing', 4, 'b', 'None', '').
+course(2, 'TMF1254', 'Communications and Computer Network', 4, 'b', 'None', '').
+course(2, 'TMF1434', 'Data_Structures_and_Algorithms', 4, 'b', 'TMF1414', 'Introduction to Programming').
+course(2, 'TMF1214', 'Computer_Architecture', 4, 'b', 'None', '').
+course(2, 'MPU3422', 'Systematic_Innovation_and_Innovative_Problem_Solving', 2, 'b', 'None', '').
+course(2, 'PPD1032', 'Credited_Co-Curricular', 2, 'b', 'None', '').
+course(2, 'PBI1102', 'English_for_Professional_Communication', 2, 'b', 'PBI1102', 'Academic_English_1').
+course(3, 'TMF2034', 'Database_Concept_and_Design', 4, 'b', 'None', '').
+course(3, 'TME2413', 'Object_Oriented_Software_Development', 3, 'b', 'TMF1434', 'Data_Structures_and_Algorithms').
+course(3, 'TME2104', 'Web_Based_System_Development', 4, 'b', 'None', '').
+course(3, 'TMF2234', 'Operating_Systems', 4, 'b', 'None', '').
+course(3, 'MPU3122', 'Islamic_and_Asian_Civilization', 2, 'b', 'None', '').
+course(4, 'TME2133', 'Software_Requirements_Engineering', 4, 'b', 'None', '').
+course(4, 'TMF2954', 'Java_Programming', 4, 'b', 'TMF1434', 'Data_Structures_and_Algorithms').
+course(4, 'TME2073', 'Intelligent_Systems', 4, 'b', 'TMF1434', 'Data_Structures_and_Algorithms').
+course(4, 'ABCXXXX', 'Elective_I', 3, 'b', 'None', '').
+course(4, 'MPU3112', 'Ethnic_Relationship_in_Malaysia', 2, 'b', 'None', '').
+course(5, 'TMF3113', 'Project_Management', 3, 'b', 'None', '').
+course(5, 'TMF3012', 'Ethics_and_Professionalism', 2, 'b', 'None', '').
+course(5, 'TME3033', 'Expert_Systems', 3, 'b', 'TME2073', 'Intelligent_Systems').
+course(5, 'TME3413', 'Software_Engineering_Laboratory', 3, 'b', 'TME2413', 'Object_Oriented_Software_Development').
+course(5, 'TME3423', 'Human_Computer_Interaction', 3, 'b', 'None', '').
+course(5, 'ABCXXXX', 'Elective_II', 3, 'b', 'None', '').
+course(5, 'MPU3312', 'Malay_Language', 3, 'b', 'None', '').
+course(6, 'TMF39412', 'Industrial_Training', 12, 'b', 'TMF3012', 'Ethics_and_Professionalism').
+course(7, 'TMF4034', 'Technopreneurship_and_Product_Development', 4, 'b', 'None', '').
+course(7, 'TME4013', 'Formal_Methods', 3, 'b', 'TMF1814', 'Discrete Mathematics').
+course(7, 'TME4113', 'Data_Mining', 3, 'b', 'None', '').
+course(7, 'TMF4913', 'Final_Year_Project_I', 3, 'b', 'TMF3113', 'Project_Management').
+course(7, 'ABCXXXX', 'Elective_III', 3, 'b', 'None', '', 'None', '').
+course(8, 'TME4093', 'Advanced_Topics_in_Software_Engineering', 3, 'b', 'None', '').
+course(8, 'TMF4033', 'Software_Testing', 3, 'b', 'None', '').
+course(8, 'TME4433', 'Computer_Security', 3, 'b', 'None', '').
+course(8, 'TMF4935', 'Final_Year_Project_II', 5, 'b', 'TMF4913', 'Final_Year_Project_I').
+
+% Facts for Band 4, 5, 6
+course(1, 'TMF1414', 'Introduction to Programming', 4, 'c', 'None', '').
+course(1, 'TMF1814', 'Discrete Mathematics', 4, 'c', 'None', '').
+course(1, 'TMF1014', 'System Analysis and Design', 4, 'c', 'None', '').
+course(1, 'MPU3222', 'Foundation of Entrepreneurship Cultivation', 2, 'c', 'None', '').
+course(1, 'PPD1041', 'Softskill and Basic Volunteerism', 1, 'c', 'None', '').
+course(1, 'PBI1092', 'Academic English 2', 2, 'c', 'None', '').
+course(2, 'TMF1874', 'Mathematics for Computing', 4, 'c', 'None', '').
+course(2, 'TMF1254', 'Communications and Computer Network', 4, 'c', 'None', '').
+course(2, 'TMF1434', 'Data_Structures_and_Algorithms', 4, 'c', 'TMF1414', 'Introduction to Programming').
+course(2, 'TMF1214', 'Computer_Architecture', 4, 'c', 'None', '').
+course(2, 'MPU3422', 'Systematic_Innovation_and_Innovative_Problem_Solving', 2, 'c', 'None', '').
+course(2, 'PPD1032', 'Credited_Co-Curricular', 2, 'c', 'None', '').
+course(2, 'PBI1082', 'English for Occupational Purposes', 2, 'c', 'PBI1092', 'Academic English 2').
+course(3, 'TMF2034', 'Database_Concept_and_Design', 4, 'c', 'None', '').
+course(3, 'TME2413', 'Object_Oriented_Software_Development', 3, 'c', 'TMF1434', 'Data_Structures_and_Algorithms').
+course(3, 'TME2104', 'Web_Based_System_Development', 4, 'c', 'None', '', 'None', '').
+course(3, 'TMF2234', 'Operating_Systems', 4, 'c', 'None', '').
+course(3, 'MPU3122', 'Islamic_and_Asian_Civilization', 2, 'c', 'None', '').
+course(4, 'TME2133', 'Software_Requirement_Engineering', 4, 'c', 'None', '').
+course(4, 'TMF2954', 'Java_Programming', 4, 'c', 'TMF1434', 'Data_Structures_and_Algorithms').
+course(4, 'TME2073', 'Intelligent_Systems', 4, 'c', 'TMF1434', 'Data_Structures_and_Algorithms').
+course(4, 'ABCXXXX', 'Elective_I', 3, 'c', 'None', '').
+course(4, 'MPU3112', 'Ethnic_Relationship_in_Malaysia', 2, 'c', 'None', '').
+course(5, 'TMF3113', 'Project_Management', 3, 'c', 'None', '').
+course(5, 'TMF3012', 'Ethics_and_Professionalism', 2, 'c', 'None', '').
+course(5, 'TME3033', 'Expert_Systems', 3, 'c', 'TME2073', 'Intelligent_Systems').
+course(5, 'TME3413', 'Software_Engineering_Laboratory', 3, 'c', 'TME2413', 'Object_Oriented_Software_Development').
+course(5, 'TME3423', 'Human_Computer_Interaction', 3, 'c', 'None', '').
+course(5, 'ABCXXXX', 'Elective_II', 3, 'c', 'None', '').
+course(5, 'MPU3312', 'Malay_Language', 3, 'c', 'None', '').
+course(6, 'TMF39412', 'Industrial_Training', 12, 'c', 'TMF3012', 'Ethics_and_Professionalism').
+course(7, 'TMF4034', 'Technopreneurship_and_Product_Development', 4, 'c', 'None', '').
+course(7, 'TME4013', 'Formal_Methods', 3, 'c', 'TMF1814', 'Discrete Mathematics').
+course(7, 'TME4113', 'Data_Mining', 3, 'c', 'None', '').
+course(7, 'TMF4913', 'Final_Year_Project_I', 3, 'c', 'TMF3113', 'Project_Management').
+course(7, 'ABCXXXX', 'Elective_III', 3, 'c', 'None', '').
+course(8, 'TME4093', 'Advanced_Topics_in_Software_Engineering', 3, 'c', 'None', '').
+course(8, 'TMF4033', 'Software_Testing', 3, 'c', 'None', '').
+course(8, 'TME4433', 'Computer_Security', 3, 'c', 'None', '').
+course(8, 'TMF4935', 'Final_Year_Project_II', 5, 'c', 'TMF4913', 'Final_Year_Project_I').
+
+sum([],0).
+
+
+% Initialize program
+start:-
+  mainMenu.
+
+
+% Add all elements in a list
+sum([H|T], N):-
+  sum(T, M),
+  N is M + H.
+
+
+% Display menu to user
+mainMenu:-
+  cls, nl, nl,
+  write('Hello, Welcome to FCSIT Intelligent Course Advisor!'),nl,nl,
+  write('---------------------------------------------------'),nl,
+  write('1. Show list of available courses for the semester'),nl,
+  write('2. Consult how many courses/credits to take for the semester'),nl,
+  write('3. Check courses prequisites'),nl,
+  write('4. Check best courses to take for the semester'),nl,
+  write('5. Exit the program'),nl,
+  write('---------------------------------------------------'),nl,nl,
+  write('Enter your choice: '),
+  read(Choice),
+  option(Choice).
+
+
+% Show list of available courses based on semester
+option(1):-
+  repeat,
+  cls, nl, nl,
+  write('Enter Semester [1-8]: '),nl,
+  read(Semester),nl,
+  (Semester == 1; Semester == 2; Semester == 3; Semester == 4; Semester == 5; Semester == 6; Semester == 7; Semester == 8 -> !;  incorrect, fail),
+  repeat,
+  cls, nl, nl,
+  write('MUET Band'),nl,
+  write('---------'), nl, nl,
+  write('a. Band 1, 2'), nl,
+  write('b. Band 3'), nl,
+  write('c. Band 4, 5, 6'), nl, nl,
+  write('Enter MUET Band [a|b|c]: '),
+  read(Muet),nl,
+  (Muet == 'a'; Muet == 'b'; Muet == 'c' -> !;  incorrect, fail),
+  cls, nl, nl,
+  write('--------------------------------------------'), nl,
+  write('The list of available courses for Semester '), write(Semester), nl,
+  write('--------------------------------------------'), nl, nl,
+  forall(course(Semester, X, Y, _, Muet, _, _), (write(X), write(' '), write(Y), nl)), nl, abort.
+
+
+% Consult how many courses or credits based on semester and muet band
+option(2):-
+  repeat,
+  cls, nl, nl,
+  write('Enter Semester [1-8]: '),nl,
+  read(Semester),nl,
+  (Semester == 1; Semester == 2; Semester == 3; Semester == 4; Semester == 5; Semester == 6; Semester == 7; Semester == 8 -> !;  incorrect, fail),
+  repeat,
+  cls, nl, nl,
+  write('MUET Band'),nl,
+  write('---------'), nl, nl,
+  write('a. Band 1, 2'), nl,
+  write('b. Band 3'), nl,
+  write('c. Band 4, 5, 6'), nl, nl,
+  write('Enter MUET Band [a|b|c]: '),
+  read(Muet),nl,
+  (Muet == 'a'; Muet == 'b'; Muet == 'c' -> !;  incorrect, fail),
+  cls, nl, nl,
+  write('---------------------------------------------------'),nl,
+  write('Courses that You Should Take'),nl,
+  write('---------------------------------------------------'),nl,nl,
+  forall(course(Semester, X, Y, Z, Muet, _, _), (write('Course: '), write(X), write(' '), write(Y), nl,
+  write('Credit Hours: '), write(Z), nl,nl)),nl,nl,
+  aggregate_all(count, course(Semester, X, Y, Z, Muet, _, _), Count),
+  write('You need to take '), write(Count), write(' courses '),
+  findall(Z, course(Semester, X, Y, Z, Muet, _, _), A),
+  sum(A, N),
+  write('with a total of '), write(N), write(' credit hours.'), nl, abort.
+
+
+% Check courses prequisites based on semester and muet band
+option(3):-
+  repeat,
+  cls, nl, nl,
+  write('Enter Semester [1-8]: '),nl,
+  read(Semester),nl,
+  (Semester == 1; Semester == 2; Semester == 3; Semester == 4; Semester == 5; Semester == 6; Semester == 7; Semester == 8 -> !;  incorrect, fail),
+  repeat,
+  cls, nl, nl,
+  write('MUET Band'),nl,
+  write('---------'), nl, nl,
+  write('a. Band 1, 2'), nl,
+  write('b. Band 3'), nl,
+  write('c. Band 4, 5, 6'), nl, nl,
+  write('Enter MUET Band [a|b|c]: '),
+  read(Muet),nl,
+  (Muet == 'a'; Muet == 'b'; Muet == 'c' -> !;  incorrect, fail),
+  cls, nl, nl,
+  write('List of Courses for Semester '), write(Semester), write(' and their prerequisites'), nl,
+  write('------------------------------------------------------'),nl, nl,
+  forall(course(Semester, X, Y, _, Muet, Z, A), (write('Course: '), write(X), write(' '), write(Y), nl,
+  write('Prerequisite: '),write(Z), write(' '), write(A), nl, nl)), nl, abort.
+
+
+% Check best courses to take based on semester
+option(4):-
+  repeat,
+  cls, nl, nl,
+  write('Enter Semester [1-8]: '),nl,
+  read(Semester),nl,
+  (Semester == 1; Semester == 2; Semester == 3; Semester == 4; Semester == 5; Semester == 6; Semester == 7; Semester == 8 -> !;  incorrect, fail),
+  repeat,
+  cls, nl, nl,
+  write('MUET Band'),nl,
+  write('---------'), nl, nl,
+  write('a. Band 1, 2'), nl,
+  write('b. Band 3'), nl,
+  write('c. Band 4, 5, 6'), nl, nl,
+  write('Enter MUET Band [a|b|c]: '),
+  read(Muet),nl,
+  (Muet == 'a'; Muet == 'b'; Muet == 'c' -> !;  incorrect, fail),
+  repeat,
+  cls, nl, nl,
+  write('Enter Previous GPA [E.g. 3.50]: '),nl,
+  read(Gpa),nl,
+  (Gpa > 0, Gpa =< 4 -> !; incorrect, fail),
+  cls, nl, nl,
+  write('-------------------------------------'), nl,
+  write('Best Courses to Take for Semester '), write(Semester), nl,
+  write('-------------------------------------'), nl, nl,
+  (Gpa >= 3 -> forall(course(Semester, X, Y, _, Muet, _, _), (write(X), write(' '), write(Y), nl)), nl, nl,
+  write('It is advisable for you to take all courses in the semester.');
+  forall(course(Semester, X, Y, 3, Muet, _, _), (write(X), write(' '), write(Y), nl)),
+  forall(course(Semester, X, Y, 4, Muet, _, _), (write(X), write(' '), write(Y), nl)), nl, nl,
+  write('It is better for you to only take the program and core courses only.'),nl), nl, abort.
+
+
+% Exit the program
+option(5):-
+  cls, nl, nl,
+  halt.
+
+
+% In case of incorrect input
+option(_):-
+  incorrect, pause, mainMenu.
+
+% Pause screen for 1 second
+pause:-
+  sleep(1).
+
+
+% Display error message to screen
+incorrect:-
+  write('Incorrect input entered!').
+
+
+% Clear screen
+cls:-
+  write('\e[2J').
